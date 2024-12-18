4bfde15d0b3b35f1c4eed2410bd3c4af4ff0ec2d
Grafscorp <golyandringraf@gmail.com>
2024-12-19 00:17:47 +0300

    cleaned code

--- ui_flutter/lib/pages/group_chat_page.dart
+++ ui_flutter/lib/pages/group_chat_page.dart
@@ -9,23 +9,42 @@ class GroupChatPage extends StatefulWidget {
 
 class _GroupChatPageState extends State<GroupChatPage> {
   double screenWidth = 0;
-
+  final primColor = const Color.fromARGB(255, 44, 39, 52);
+  final secColor = const Color.fromARGB(255, 25, 21, 32);
   @override
   Widget build(BuildContext context) {
     screenWidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
+      drawer: Drawer(
+        child: Text("Test"),
+      ),
       body: Row(
         children: [
           Container(
             width: screenWidth > 350 ? 350 : 70,
             color: Color.fromARGB(255, 44, 39, 52),
-          ),
-          Expanded(
-            child: Container(
-              color: Color.fromARGB(255, 25, 21, 32),
-              width: 600,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        icon: Icon(
+                          Icons.menu,
+                          size: 40,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
+          Text("data"),
         ],
       ),
     );
