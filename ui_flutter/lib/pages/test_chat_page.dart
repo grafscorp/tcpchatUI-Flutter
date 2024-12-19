f28413f4be6f9972cd90ff1e1a7bd3bda4c0ea6e
Grafscorp <golyandringraf@gmail.com>
2024-12-19 13:18:39 +0300

    Update Test Chat

--- ui_flutter/lib/pages/test_chat_page.dart
+++ ui_flutter/lib/pages/test_chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ui_flutter/components/ChatsComponents/input_chat_container.dart';
 import 'package:ui_flutter/components/ChatsComponents/message_box.dart';
+import 'package:ui_flutter/components/ChatsComponents/messages_list.dart';
 
 class TestChatPage extends StatefulWidget {
   const TestChatPage({super.key});
@@ -20,11 +21,11 @@ class _TestChatPageState extends State<TestChatPage> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            MessageBox(
-              messageText: "Test",
-            ),
+            Expanded(child: MessagesLists()),
             InputChatContainer(),
           ],
         ),
